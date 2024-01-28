4b51aa56b423a9d599a78173b41fa4368950521e
Janjre <theshepherds555@gmail.com>
2024-01-28 11:44:12 +0000

    THings

--- Modules/WorldEdit with undo.lua
+++ Modules/WorldEdit with undo.lua
@@ -23,7 +23,7 @@ description = "World Edit for bedrock. Type .WEhelp for an explanation (The wand
 --    do return true end
 --end
 
---wedit for onix made by @Janjre (github name) mc username = 
+--wedit for onix made by @Janjre (github name) mc username = HusdhedCandle753
 function sleep(n)  -- seconds
     local clock = os.clock
    local t0 = clock()
