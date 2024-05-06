bc16c1d5b2ded700de0afee32fef6ee3ff34ca33
Janjre <theshepherds555@gmail.com>
2024-05-06 12:44:04 +0100

    adding some direction things

--- Modules/ImgBuild.lua
+++ Modules/ImgBuild.lua
@@ -1,5 +1,5 @@
 
-name = "Image Generator jqms"
+name = "Image Generator"
 description = "Change the image file \"toBuild.png\" to the image and then change the size settings"
 
 importLib("BlockRGB.lua")
@@ -14,10 +14,14 @@ importLib("BlockRGB.lua")
 client.settings.addInfo("Make an image file called toBuild.png of whatever you want to build, then do .buildCoords \nto set where it will build, then set how tall you want it to be, it will automatically find \nout the width. Run .build and it will Make it!")
 imageHeight = 600
 imageHeightSet = client.settings.addInt("Image Height","imageHeight",1,500)
+buildVertical = false
+client.settings.addBool("Build vertical","buildVertical")
 imageWidth = 600
+zWall = true
+fixZSet = client.settings.addBool("Fixed Z","zWall")
 -- client.settings.addInfo("Image width (updates when you close and reopen)")
 -- abc = client.settings.addInfo("imageWidth")
-img = gfx2.loadImage("toBuild.png") -- loads the img
+-- img = gfx2.loadImage("toBuild.png") -- loads the img
 
 -- client.settings.addKeybind("Build key","buildKey")
 img = nil
@@ -35,13 +39,6 @@ registerCommand("build", function ()
     local widthStep = img.width / imageWidth
 
     
-
-    client.execute("execute tickingarea add circle ~ ~ ~ 4")
-    client.execute("execute tickingarea add circle ~ ~ ~128 4")
-    client.execute("execute tickingarea add circle ~ ~ ~-128 4")
-    client.execute("execute tickingarea add circle ~128 ~ ~ 4")
-    client.execute("execute tickingarea add circle ~-128 ~ ~ 4")
-
     client.execute("execute gamerule sendcommandfeedback false")
 
     for i = 1, img.height, heightStep do
@@ -51,13 +48,18 @@ registerCommand("build", function ()
                 local curBlock2 = RGBtoBlock(curColour2.r, curColour2.g, curColour2.b)
                 local offsetX = math.floor((j - img.width / 2) / widthStep)
                 local offsetZ = math.floor((i - img.height / 2) / heightStep)
-                client.execute("execute setblock " .. (xStartCoord + offsetX) .. " " .. (yStartCoord) .. " " .. (zStartCoord + offsetZ) .. " " .. curBlock2)
+                if buildVertical == false then
+                    client.execute("execute setblock " .. (xStartCoord + offsetX) .. " " .. (yStartCoord) .. " " .. (zStartCoord + offsetZ) .. " " .. curBlock2)
+                elseif zWall == true then
+                    client.execute("execute setblock " .. (xStartCoord - offsetX) .. " " .. (yStartCoord - offsetZ) .. " " .. (zStartCoord) .. " " .. curBlock2)
+                else
+                    client.execute("execute setblock " .. (xStartCoord) .. " " .. (yStartCoord - offsetZ) .. " " .. (zStartCoord - offsetX) .. " " .. curBlock2)
+                end
             end
         end
     end
 
     client.execute("execute gamerule sendcommandfeedback true")
-    client.execute("execute tickingarea remove_all")
 end)
 
 needsUnload = false
@@ -78,21 +80,62 @@ function render()
 end
 
 function render3d()
-
     gfx.tcolor(255, 255, 255, 200)
-    if xStartCoord then
-        local offsetX = (img.width / 2) * imageScale
-        local offsetZ = (img.height / 2) * imageScale
-        gfx.tquad(
-            xStartCoord - offsetX, yStartCoord, zStartCoord + offsetZ,
-            0, 1,
-            xStartCoord + offsetX, yStartCoord, zStartCoord + offsetZ,
-            1, 1,
-            xStartCoord + offsetX, yStartCoord, zStartCoord - offsetZ,
-            1, 0,
-            xStartCoord - offsetX, yStartCoord, zStartCoord - offsetZ,
-            0, 0,
-            "toBuild"
-        )
+    if buildVertical == false then
+        if xStartCoord then
+            local offsetX = (img.width / 2) * imageScale
+            local offsetZ = (img.height / 2) * imageScale
+            gfx.tquad( 
+                xStartCoord - offsetX, yStartCoord, zStartCoord + offsetZ,
+                0, 1,
+                xStartCoord + offsetX, yStartCoord, zStartCoord + offsetZ,
+                1, 1,
+                xStartCoord + offsetX, yStartCoord, zStartCoord - offsetZ,
+                1, 0,
+                xStartCoord - offsetX, yStartCoord, zStartCoord - offsetZ,
+                0, 0,
+                "toBuild"
+            )
+        end
+    elseif zWall == true then
+        if xStartCoord then
+            local offsetX = (img.width / 2) * imageScale
+            local offsetZ = (img.height / 2) * imageScale
+            gfx.tquad(
+                xStartCoord + offsetX, yStartCoord - offsetZ, zStartCoord,
+                0, 1,
+                xStartCoord - offsetX, yStartCoord - offsetZ, zStartCoord,
+                1, 1,
+                xStartCoord - offsetX, yStartCoord + offsetZ, zStartCoord,
+                1, 0,
+                xStartCoord + offsetX, yStartCoord + offsetZ, zStartCoord,
+                0, 0,
+                "toBuild"
+            )
+        end
+    else
+        if xStartCoord then
+            local offsetX = (img.width / 2) * imageScale
+            local offsetZ = (img.height / 2) * imageScale
+            gfx.tquad(
+                xStartCoord, yStartCoord - offsetZ, zStartCoord + offsetX,
+                0, 1,
+                xStartCoord, yStartCoord - offsetZ, zStartCoord - offsetX,
+                1, 1,
+                xStartCoord, yStartCoord + offsetZ, zStartCoord - offsetX,
+                1, 0,
+                xStartCoord, yStartCoord + offsetZ, zStartCoord + offsetX,
+                0, 0,
+                "toBuild"
+            )
+        end
+    end
+end
+
+function update()
+    if buildVertical then
+        fixZSet.visible = true
+    else
+        fixZSet.visible = false
     end
 end
